22bf9ae66e04f3b48018d09bf361b332246e84f7
wildantisna <wildan27370@gmail.com>
2026-09-05 18:02:22 +0700

    Improve safe API error handling

--- app/src/main/java/com/project/prayerreminder/core/SafeApiCall.kt
+++ app/src/main/java/com/project/prayerreminder/core/SafeApiCall.kt
@@ -3,7 +3,6 @@ package com.project.prayerreminder.core
 import com.project.prayerreminder.core.data.remote.model.BaseResponse
 import retrofit2.HttpException
 import retrofit2.Response
-import java.io.IOException
 import kotlin.coroutines.cancellation.CancellationException
 
 suspend fun <T> safeApiCall(
@@ -20,21 +19,22 @@ suspend fun <T> safeApiCall(
                     data = body.data,
                     message = body.status ?: "Success"
                 )
+                // Handle empty body
             } else {
                 NetworkResult.Error(
-                    message = body?.status ?: "Unknown server response."
+                    message = body?.status ?: "Invalid server response.",
+                    code = body?.code ?: response.code()
                 )
             }
         } else {
             NetworkResult.Error(
-                message = ErrorMapper.map(HttpException(response))
+                message = ErrorMapper.map(HttpException(response)),
+                code = response.code()
             )
         }
     } catch (e: CancellationException) {
         throw e
     } catch (e: Exception) {
-        NetworkResult.Error(
-            message = ErrorMapper.map(e)
-        )
+        NetworkResult.Error(message = ErrorMapper.map(e))
     }
 }
