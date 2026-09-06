f104c203218c8a64812efdfa59358340e1fabbdb
wildantisna <wildan27370@gmail.com>
2026-09-06 11:19:06 +0700

    Add reusable `AppBar` composable with navigation support and back icon asset

---
+++ app/src/main/java/com/project/prayerreminder/utils/composables/AppBar.kt
@@ -0,0 +1,58 @@
+package com.project.prayerreminder.utils.composables
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.project.prayerreminder.R
+import com.project.prayerreminder.core.theme.PrayerReminderTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    modifier: Modifier = Modifier,
+    canNavigateBack: Boolean = false,
+    navigateBack: () -> Unit = {},
+    title: String,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.fillMaxWidth(1f),
+                textAlign = TextAlign.Center
+            )
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateBack) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_back_arrow),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true, name = "Appbar", device = "id:pixel_5")
+@Composable
+fun AppbarPreview() {
+    PrayerReminderTheme {
+        AppBar(title = "Prayer Reminder", canNavigateBack = true)
+    }
+}
