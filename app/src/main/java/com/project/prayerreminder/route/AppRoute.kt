3f518ada3202b6de8f68c5b860f998e407b45177
wildantisna <wildan27370@gmail.com>
2026-09-10 16:38:41 +0700

    Remove enter and exit transitions from `NavHost` in `AppRoute`

--- app/src/main/java/com/project/prayerreminder/route/AppRoute.kt
+++ app/src/main/java/com/project/prayerreminder/route/AppRoute.kt
@@ -1,7 +1,5 @@
 package com.project.prayerreminder.route
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
@@ -95,18 +93,6 @@ fun AppRoute(
                 NavHost(
                     navController = navController,
                     startDestination = AppScreen.SPLASH.route,
-                    enterTransition = {
-                        EnterTransition.None
-                    },
-                    exitTransition = {
-                        ExitTransition.None
-                    },
-                    popEnterTransition = {
-                        EnterTransition.None
-                    },
-                    popExitTransition = {
-                        ExitTransition.None
-                    },
                     modifier = Modifier
                         .padding(navigationPadding)
                         .consumeWindowInsets(navigationPadding)
